b5748670c2521ca57b29a130032042e756970f71
Greed1959JR <amarildomsj@gmail.com>
2025-04-16 20:35:46 -0300

    Update MPbot.py

--- MPbot.py
+++ MPbot.py
@@ -129,4 +129,4 @@ verificacao_thread.daemon = True
 verificacao_thread.start()
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    print("Rodando localmente. Em produção, use gunicorn.")
