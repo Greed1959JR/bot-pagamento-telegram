04487e095955bb110d7747fdf5bffd829cc31cb0
Greed1959JR <amarildomsj@gmail.com>
2025-04-18 11:55:24 -0300

    Update MPbot.py

--- MPbot.py
+++ MPbot.py
@@ -37,7 +37,7 @@ def carregar_dados():
     with lock:
         if not os.path.exists(DB_FILE):
             return {}
-       with open(DB_FILE, 'r') as f:
+        with open(DB_FILE, 'r') as f:
             return json.load(f)
 
 def salvar_dados(dados):
