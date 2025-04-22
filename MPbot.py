aac711c5d89f99ed97e3df2c9e96e3b1bffdc8fe
Greed1959JR <amarildomsj@gmail.com>
2025-04-21 21:04:18 -0300

    Update MPbot.py

--- MPbot.py
+++ MPbot.py
@@ -334,20 +334,20 @@ def webhook():
  
          elif query.data == "ajuda":
         ajuda_texto = (
-        "❓ *Ajuda do Bot*\n\n"
-        "- Para *assinar*, clique em \"💰 Pagar (Mensal)\" ou \"💰 Pagar (Trimestral)\".\n"
-        "- Para *ver os planos*, clique em \"📄 Ver Planos\".\n"
-        "- Em caso de dúvidas, envie um email para: overgeared1959@gmail.com\n"
-        "📬 Telegram: [@overgeared_tips](https://t.me/overgeared_tips)"
-    )
-    BOT.send_message(
-        chat_id=chat_id,
-        text=ajuda_texto,
-        parse_mode=telegram.ParseMode.MARKDOWN,
-        reply_markup=telegram.InlineKeyboardMarkup([
-            [telegram.InlineKeyboardButton("🔙 Voltar", callback_data="voltar_menu")]
-        ])
-    )
+                 "❓ *Ajuda do Bot*\n\n"
+                 "- Para *assinar*, clique em \"💰 Pagar (Mensal)\" ou \"💰 Pagar (Trimestral)\".\n"
+                 "- Para *ver os planos*, clique em \"📄 Ver Planos\".\n"
+                 "- Em caso de dúvidas, envie um email para: overgeared1959@gmail.com\n@overgeared_tips"
+                 "- Em caso de dúvidas, envie um email para: @overgeared_tips"
+             )
+             BOT.send_message(
+                 chat_id=chat_id,
+                 text=ajuda_texto,
+                 parse_mode=telegram.ParseMode.MARKDOWN,
+                 reply_markup=telegram.InlineKeyboardMarkup([
+                     [telegram.InlineKeyboardButton("🔙 Voltar", callback_data="voltar_menu")]
+                 ])
+             )
  
          elif query.data == "voltar_menu":
              BOT.send_message(
